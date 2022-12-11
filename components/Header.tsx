ddbed2478e47fadca6c9992408a6ecf758c97c93
🎈 <numofran6@gmail.com>
2022-12-11 08:34:35 +0000

    slide animation removed

--- components/Header.tsx
+++ components/Header.tsx
@@ -15,14 +15,14 @@ function Header({}: Props) {
 				<motion.div
 					initial={{
 						opacity: 0,
-						scale: 0.5,
+						scale: 0.8,
 					}}
 					animate={{
 						opacity: 1,
 						scale: 1,
 					}}
 					transition={{
-						duration: 1,
+						duration: 0.8,
 					}}
 				>
 					<SocialIcon
@@ -45,14 +45,14 @@ function Header({}: Props) {
 				<motion.div
 					initial={{
 						opacity: 0,
-						scale: 0.5,
+						scale: 0.8,
 					}}
 					animate={{
 						opacity: 1,
 						scale: 1,
 					}}
 					transition={{
-						duration: 1,
+						duration: 0.8,
 					}}
 					className="flex items-center"
 				>
