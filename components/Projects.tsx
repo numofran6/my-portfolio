dc4d76729dc16f17d22b6d3649c0811fbd2956ea
Francis <numofran6@gmail.com>
2023-01-06 14:13:59 +0000

    0/15

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -75,7 +75,7 @@ function Projects({}: Props) {
 										initial={{ opacity: 0, x: 50 }}
 										whileInView={{ opacity: 1, x: 0 }}
 										transition={{ duration: 1 }}
-										className="text-gray-200 font-semibold text-sm md:text-md text-left space-y-1 pl-5 flex flex-col"
+										className="text-gray-300 font-semibold text-sm md:text-md text-left space-y-1 pl-5 flex flex-col"
 									>
 										{project.description[0] && (
 											<span>&bull; {project.description[0]}</span>
